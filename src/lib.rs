a211be7dd4a859c791827651d8639100db52ef61
griffi-gh <prasol258@gmail.com>
2023-05-03 21:28:36 +0200

    It won't inline, so i added `inline(always)`

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ pub trait Twiddle<T> {
 macro_rules! impl_twiddle {
 	($tt: tt) => {
 		::paste::paste! {
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _unwrap_or>](option: Option<$tt>, or: $tt) -> $tt {
 				match option {
 					Some(x) => x,
@@ -41,34 +41,34 @@ macro_rules! impl_twiddle {
 				}
 			}
 	
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _cshl>](x: $tt, n: u32) -> $tt {
 				[<$tt _unwrap_or>](x.checked_shl(n), 0)
 			}
 	
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _mask>](start: u32, end: u32) -> $tt {
 				let top = [<$tt _cshl>](1, 1 + start - end);
 				top.wrapping_sub(1) << end
 			}
 	
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _bits>](x: $tt, start: u32, end: u32) -> $tt {
 				(x & [<$tt _mask>](start, end)) >> end
 			}
 
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _bit>](x: $tt, bit: u32) -> bool {
 				((x >> bit) & 1) != 0
 			}
 
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _set_bits>](x: $tt, start: u32, end: u32, bits: $tt) -> $tt {
 				let mask = [<$tt _mask>](start, end);
         (x & !mask) | ((bits << end) & mask)
 			}
 
-			#[inline]
+			#[inline(always)]
 			const fn [<$tt _set_bit>](x: $tt, bit: u32, value: bool) -> $tt {
 				let mask = [<$tt _mask>](bit, bit);
 				(x & !mask) | ((value as $tt) << bit)
@@ -80,7 +80,7 @@ macro_rules! impl_twiddle {
 			pub struct [<Const $tt:upper>](pub $tt);
 
 			impl From<$tt> for [<Const $tt:upper>] {
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn from(x: $tt) -> Self {
 					Self(x)
@@ -88,7 +88,7 @@ macro_rules! impl_twiddle {
 			}
 
 			impl From<[<Const $tt:upper>]> for $tt {
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn from(x: [<Const $tt:upper>]) -> Self {
 					x.0
@@ -97,35 +97,35 @@ macro_rules! impl_twiddle {
 
 			impl [<Const $tt:upper>] {
 				/// Creates a bitmask from a range of bits.
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				pub const fn mask(range: RangeInclusive<u32>) -> $tt {
 					[<$tt _mask>](*range.start(), *range.end())
 				}
 
 				/// Returns a set of bits.
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				pub const fn bits(self, range: RangeInclusive<u32>) -> $tt {
 					[<$tt _bits>](self.0, *range.start(), *range.end())
 				}
 				
 				/// Returns a given bit as a boolean.
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				pub const fn bit(self, bit: u32) -> bool {
 					[<$tt _bit>](self.0, bit)
 				}
 
 				/// Replace a set of bits
-				#[inline]
+				#[inline(always)]
 				#[must_use] 
 				pub const fn set_bits(self, range: RangeInclusive<u32>, value: $tt) -> $tt {
 					[<$tt _set_bits>](self.0, *range.start(), *range.end(), value)
 				}
 
 				/// Set a single bit
-				#[inline]
+				#[inline(always)]
 				#[must_use] 
 				pub const fn set_bit(self, bit: u32, value: bool) -> $tt {
 					[<$tt _set_bit>](self.0, bit, value)
@@ -133,31 +133,31 @@ macro_rules! impl_twiddle {
 			}
 
       impl Twiddle<$tt> for [<Const $tt:upper>] {
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn mask(range: RangeInclusive<u32>) -> $tt {
 					Self::mask(range)
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn bits(self, range: RangeInclusive<u32>) -> $tt {
 					self.bits(range)
 				}
 				
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn bit(self, bit: u32) -> bool {
 					self.bit(bit)
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use] 
 				fn set_bits(self, range: RangeInclusive<u32>, value: $tt) -> $tt {
 					self.set_bits(range, value)
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use] 
 				fn set_bit(self, bit: u32, value: bool) -> $tt {
 					self.set_bit(bit, value)
@@ -165,31 +165,31 @@ macro_rules! impl_twiddle {
 			}
 
 			impl Twiddle<$tt> for $tt {
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn mask(range: RangeInclusive<u32>) -> $tt {
 					[<$tt _mask>](*range.start(), *range.end())
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn bits(self, range: RangeInclusive<u32>) -> $tt {
 					[<$tt _bits>](self, *range.start(), *range.end())
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use]
 				fn bit(self, bit: u32) -> bool {
 					[<$tt _bit>](self, bit)
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use] 
 				fn set_bits(self, range: RangeInclusive<u32>, value: $tt) -> $tt {
 					[<$tt _set_bits>](self, *range.start(), *range.end(), value)
 				}
 
-				#[inline]
+				#[inline(always)]
 				#[must_use] 
 				fn set_bit(self, bit: u32, value: bool) -> $tt {
 					[<$tt _set_bit>](self, bit, value)
